73211e492af7b3103dc1da88134f8647adab5947
gitterchris <Chris@192.168.2.102>
2014-03-29 11:52:27 +0400

    Validation for the Login activity

--- src/com/crime/crimebusters/LoginActivity.java
+++ src/com/crime/crimebusters/LoginActivity.java
@@ -2,7 +2,6 @@ package com.crime.crimebusters;
 
 import java.util.concurrent.ExecutionException;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,7 +13,11 @@ import android.widget.Toast;
 
 import com.crime.crimebusters.login.Login;
 
-@SuppressLint("ShowToast")
+/**
+ * Activity for the Login page.
+ * @author Chris
+ *
+ */
 public class LoginActivity extends Activity {
 
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -32,18 +35,26 @@ public class LoginActivity extends Activity {
 		return true;
 	}
 	
-	/** Called when the user clicks the Send button 
+	/** 
+	 * Event handler for the login button 
 	 * @throws ExecutionException 
 	 * @throws InterruptedException */
-	public void sendMessage(View view) throws InterruptedException, ExecutionException {
-		Button buttonLogin = (Button) findViewById(R.id.sign_in_button);
-		buttonLogin.setText("Logging in...");
-		
+	public void sendMessage(View view) throws InterruptedException, ExecutionException {		
 		EditText editUserName = (EditText) findViewById(R.id.email);
 		String userName = editUserName.getText().toString();
 		
 		EditText editPassword = (EditText) findViewById(R.id.password);
 		String password = editPassword.getText().toString();
+		
+		if (!validateFields(userName, password)) {
+			Toast.makeText(this, 
+					"Username and password required.", 
+					Toast.LENGTH_LONG).show();	
+			return;
+		}
+		
+		Button buttonLogin = (Button) findViewById(R.id.sign_in_button);
+		buttonLogin.setText("Logging in...");
 		
 		Login login = new Login(buttonLogin);
 		String loginStatus = login.validateCredentials(userName, password);
@@ -57,6 +68,30 @@ public class LoginActivity extends Activity {
 		}
 	}
 	
+	/**
+	 * Validates the userName and password before sending a web service call.
+	 * @param userName userName of the user.
+	 * @param password password of the user
+	 * @return
+	 */
+	private boolean validateFields(String userName, String password) {
+		if (isFieldEmpty(userName, password)) {
+			return false;
+		}
+		
+		return true;
+	}
+
+	/**
+	 * Validates for empty fields.
+	 * @param userName userName of the user
+	 * @param password password of the user
+	 * @return true if one field is empty
+	 */
+	private boolean isFieldEmpty(String userName, String password) {
+		return userName.equals("") || password.equals("");
+	}
+
 	/**
 	 * Starts the create account activity
 	 * @param view
